57fed05b7d16811653c9d955dba413db7658257c
ayon chowdhury <aayonchowdhuryy@gmail.com>
2025-03-13 11:04:32 +0600

    good morning

---
+++ DIU/binary search/binary_search3.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define Yes cout << "Yes\n"
+#define YES cout << "YES\n"
+#define yes cout << "yes\n"
+#define No cout << "No\n"
+#define NO cout << "NO\n"
+#define no cout << "no\n"
+#define ll long long int
+
+int main(){
+    op();
+    ll n;
+    cin >> n;
+    while (n--)
+    {
+        solve();
+    }
+    
+
+    return 0;
+}
